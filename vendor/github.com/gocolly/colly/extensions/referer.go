6fdb35ef811e8841d5743ce68f5c3cdd64e32a16
unstppbl <altsab@yandex.com>
2020-01-27 12:45:35 +0600

    update apps.json, add go modules

---
+++ vendor/github.com/gocolly/colly/extensions/referer.go
@@ -0,0 +1,19 @@
+package extensions
+
+import (
+	"github.com/gocolly/colly"
+)
+
+// Referer sets valid Referer HTTP header to requests.
+// Warning: this extension works only if you use Request.Visit
+// from callbacks instead of Collector.Visit.
+func Referer(c *colly.Collector) {
+	c.OnResponse(func(r *colly.Response) {
+		r.Ctx.Put("_referer", r.Request.URL.String())
+	})
+	c.OnRequest(func(r *colly.Request) {
+		if ref := r.Ctx.Get("_referer"); ref != "" {
+			r.Headers.Set("Referer", ref)
+		}
+	})
+}
